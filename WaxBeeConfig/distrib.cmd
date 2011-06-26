c229fe46562410fea412580a9a62165a717b11ab
bernard3000@gmail.com <bernard3000@gmail.com@455f4b3d-0d0e-9ffe-2068-a16b02132a71>
2011-06-26 04:58:57 +0000

    automate building the distribution zip (windows cmd file) add a readme.html

---
+++ WaxBeeConfig/distrib.cmd
@@ -0,0 +1,48 @@
+@echo off
+ECHO.
+
+IF "%JAVA_HOME%" == "" set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_23
+
+set JAR_EXE=%JAVA_HOME%\bin\jar.exe
+
+IF NOT EXIST "%JAR_EXE%" GOTO ERROR
+
+set ROOT=%~dp0.
+set DISTRIBDIR=%ROOT%\distrib
+set AVRPROJECTROOT=%ROOT%\..\WaxB_Adb2Usb
+
+rem cleanup the last distrib and start fresh
+rd "%DISTRIBDIR%" /s /q 
+mkdir "%DISTRIBDIR%"
+
+rem waxbee.jar
+
+mkdir "%DISTRIBDIR%\jar"
+xcopy "%ROOT%\bin\org" "%DISTRIBDIR%\jar\org\" /E > nul
+copy "%ROOT%\teensylib\teensy64.dll" "%DISTRIBDIR%\jar\" > nul
+copy "%ROOT%\teensylib\teensy32.dll" "%DISTRIBDIR%\jar\" > nul
+copy "%ROOT%\rawhidlib\rawhid64.dll" "%DISTRIBDIR%\jar\" > nul
+copy "%AVRPROJECTROOT%\Release\WaxB_Adb2Usb.hex" "%DISTRIBDIR%\jar\" > nul
+
+rem final package to zipping
+mkdir "%DISTRIBDIR%\waxbee"
+xcopy "%ROOT%\config_templates" "%DISTRIBDIR%\waxbee\config_templates\" /E > nul 
+copy "%ROOT%\miglayout*.jar" "%DISTRIBDIR%\waxbee" > nul
+copy "%ROOT%\readme.html" "%DISTRIBDIR%\waxbee" > nul
+
+rem build the waxbee.jar application
+"%JAR_EXE%" cfme0 "%DISTRIBDIR%\waxbee\waxbee.jar" "%ROOT%\manifest.txt" org.waxbee.WaxBee -C "%DISTRIBDIR%\jar" .
+
+rem build the final zip
+"%JAR_EXE%" cfM "%DISTRIBDIR%\waxbee.zip" -C "%DISTRIBDIR%" waxbee
+
+ECHO final distribution zip:  "%DISTRIBDIR%\waxbee.zip"
+
+goto END
+
+:ERROR
+
+ECHO Properly set JAVA_HOME to point to a Java JDK 1.6 or later
+ECHO.
+
+:END
